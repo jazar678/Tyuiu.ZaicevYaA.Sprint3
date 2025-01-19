eba4695990afa311163d2311de5af91fee82ac1f
Redeks Gg <ddredeks@gmail.com>
2025-01-20 00:08:34 +0500

    jgfx

---
+++ Tyuiu.ZaicevYaA.Sprint3.Task6.V23.Lib/Class1.cs
@@ -0,0 +1,28 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.ZaicevYaA.Sprint3.Task6.V23.Lib
+{
+    public class DataService : ISprint3Task6V23
+    {
+        public int GetSumTheDivisors(int startValue, int stopValue)
+        {
+            int qw = 0;
+            int count = 0;
+            for (int i = startValue; i <= stopValue; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        count++;
+                    }
+
+                }
+                if (count < 11)
+                {
+                    qw++;
+                }
+            }
+            return qw;
+        }
+    }
+}
---
+++ Tyuiu.ZaicevYaA.Sprint3.Task6.V23/Program.cs
@@ -0,0 +1,35 @@
+﻿using Tyuiu.ZaicevYaA.Sprint3.Task6.V23.Lib;
+namespace Tyuiu.ZaicevYaA.Sprint3.Task6.V23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Title = "Спринт | Выполнил: Зайцев Я.А. | ПКТб-24-1";
+            Console.WriteLine("*****************************************************************");
+            Console.WriteLine("* Спринт #3                                                     *");
+            Console.WriteLine("* Тема: Алгоритмы циклической структуры                         *");
+            Console.WriteLine("* Задание #5                                                    *");
+            Console.WriteLine("* Вариант #20                                                   *");
+            Console.WriteLine("* Выполнил : Зайцев Ярослав Александрович | ПКТб-24-1           *");
+            Console.WriteLine("*****************************************************************");
+            Console.WriteLine("* УСЛОВИЕ :                                                     *");
+            Console.WriteLine("*На отрезке, где x принимает значения от -5 до 5                *");
+            Console.WriteLine("*  вычислить значение функции y=sin(x)/x                        *");
+            Console.WriteLine("*                                                               *");
+            Console.WriteLine("*****************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ :                                             *");
+            Console.WriteLine("*****************************************************************");
+            DataService dataService = new DataService();
+            int startValue = 18;
+            int stopValue = 28;
+
+            Console.WriteLine("*****************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                    *");
+            Console.WriteLine("*****************************************************************");
+            Console.WriteLine("ответ " + dataService.GetSumTheDivisors(startValue,stopValue));
+            Console.ReadKey();
+        }
+    }
+}
