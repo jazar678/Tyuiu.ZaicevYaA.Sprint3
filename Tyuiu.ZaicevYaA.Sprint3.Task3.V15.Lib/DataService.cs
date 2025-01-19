3dadc83b3c1b34ded4dc7a40a826865a48b5f287
Redeks Gg <ddredeks@gmail.com>
2025-01-19 22:57:57 +0500

    yjfgdt6i

--- Tyuiu.ZaicevYaA.Sprint3.Task3.V15.Lib/DataService.cs
+++ Tyuiu.ZaicevYaA.Sprint3.Task3.V15.Lib/DataService.cs
@@ -1,11 +1,11 @@
 ﻿using tyuiu.cources.programming.interfaces.Sprint3;
 namespace Tyuiu.ZaicevYaA.Sprint3.Task3.V15.Lib
 {
-    public class DataService : ISprint3Task3V30
+    public class DataService : ISprint3Task3V15
     {
-        public int GetMaxCharCount(string value, char item)
+        public int GetMinCharCount(string value, char item)
         {
-            int count =0;
+            int count = 0;
             foreach (char c in value)
             {
                 if (c == item)
