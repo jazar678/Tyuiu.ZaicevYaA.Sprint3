02b35b6fae0f8e6e94d67402ab19f39ed6997204
Redeks Gg <ddredeks@gmail.com>
2025-01-19 21:32:00 +0500

    q

---
+++ Tyuiu.ZaicevYaA.Sprint3.Task2.V30.Test/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.ZaicevYaA.Sprint3.Task2.V30.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
