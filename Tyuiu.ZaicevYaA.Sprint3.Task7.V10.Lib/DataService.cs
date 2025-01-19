6c9015ca8b4284c6fce1771ae8f8c9b51f172002
Redeks Gg <ddredeks@gmail.com>
2025-01-20 00:39:02 +0500

    fw

--- Tyuiu.ZaicevYaA.Sprint3.Task7.V10.Lib/DataService.cs
+++ Tyuiu.ZaicevYaA.Sprint3.Task7.V10.Lib/DataService.cs
@@ -8,7 +8,7 @@ namespace Tyuiu.ZaicevYaA.Sprint3.Task7.V10.Lib
             int er = 0;
             int s = stopValue - startValue + 1;
             double[] result = new double[s];
-            for (int i = startValue; i < stopValue; i++) 
+            for (int i = startValue; i <= stopValue; i++) 
             {
                 result[er] = ((5*i + 2.5)/(Math.Sin(i) + 3))+ 2 * i + Math.Cos(i);
                 if (double.IsInfinity(result[er]) || double.IsNaN(result[er]))
